b5e0bebf30cb2ab602a3b91f21353c57a4325959
zarko5 <zarkohajder1@gmail.com>
2021-05-22 20:16:55 +0200

    update

--- 1.py
+++ 1.py
@@ -5,6 +5,8 @@ from pygame.time import delay
 import pygamebg
 from array import *
 import numpy as np
+import random
+
 
 br_semafora=10
 br_car=10
@@ -14,55 +16,59 @@ class Car(object):
         self.image = pg.image.load("./car.png").convert_alpha()
         self.x = 0
         self.y = 0
-        self.image = pg.transform.rotate(self.image,270)
+        self.image = pg.transform.rotate(self.image,90)
     def draw(self, surface):
         surface.blit(self.image, (self.x, self.y))
 
-class Road(object):  
+class Road(object):     
     def __init__(self):
         self.image = pg.image.load("./road.png").convert_alpha()
         self.x = 0
         self.y = 0
-       # self.image = pg.transform.rotate(self.image,270)
+        #self.image = pg.transform.rotate(self.image,270)
     def draw(self, surface):
         surface.blit(self.image, (self.x, self.y))
 
 fps = 45
-semafor = array('I',[110,10,200,50,250,300,300,350])
+semafor = array('I',[450,140,450,440,250,300,300,350])
 boje = ["Red","Red","Red","Red"]
-surface = pygamebg.open_window(1600,800 , "Traffic Jam Simulation")
+surface = pygamebg.open_window(1700,900 , "Traffic Jam Simulation")
 
 RoadArr=[[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
         [0,0,0,2,0,0,0,2,0,2,0,0,2,0,0,0],
         [0,0,0,2,0,0,0,1,1,1,0,0,2,0,0,0],
-        [1,1,1,3,1,1,1,2,0,0,0,0,2,0,0,0],
+        [1,1,1,1,1,1,1,2,0,0,0,0,2,0,0,0],
         [0,0,0,2,0,2,0,2,0,0,0,0,2,0,0,0],
         [0,0,0,2,0,1,1,1,1,1,1,1,1,1,1,1],
         [1,1,1,1,1,2,0,0,2,0,0,0,2,0,0,0],
         [0,0,0,2,0,0,0,0,2,0,0,0,2,0,0,0]]
 RoadObj  = np.zeros(shape=(8,16), dtype=Road)
-
+CarLocation = np.zeros(shape=(8,16),dtype=int)
 CarArr= np.zeros(shape=(9,17), dtype=Car)
 
 for i in range(0,7):
     for y in range(0,15):
         if RoadArr[i][y]==2 :
             RoadObj[i][y]=Road()
-            RoadObj[i][y].y=i*100 
-            RoadObj[i][y].x=y*100
-       
+            RoadObj[i][y].y=i*100+100 
+            RoadObj[i][y].x=y*100+100
+            #CarArr[i][y] = Car()
+            #CarArr[i][y].x=i*100
+            #CarArr[i][y].y=y*100+25
         elif RoadArr[i][y]==1:
             RoadObj[i][y]=Road()
-            RoadObj[i][y].y=i*100 
-            RoadObj[i][y].x=y*100 
+            RoadObj[i][y].y=i*100 +100
+            RoadObj[i][y].x=y*100 + 100
             RoadObj[i][y].image=pg.transform.rotate(RoadObj[i][y].image,90)
-            CarArr[i][y] = Car()
-            CarArr[i][y].x=i*100
-            CarArr[i][y].y=y*100+25
-        elif RoadArr[i][y] == 3:
-            RoadObj[i][y]=Road()
-            RoadObj[i][y].y=i*100 
-            RoadObj[i][y].x=y*100 
+            if round(random.random()):
+                CarLocation[i][y]=1
+                CarArr[i][y] = Car()
+                CarArr[i][y].x=y*100+100
+                CarArr[i][y].y=i*100+130
+       # elif RoadArr[i][y] == 3:
+        #    RoadObj[i][y]=Road()
+         #   RoadObj[i][y].y=i*100
+          #  RoadObj[i][y].x=y*100 
             
         
 #car.y=15
@@ -76,30 +82,30 @@ while Run:
         for y in range(0,15):
             if RoadArr[i][y]!=0 :
                 RoadObj[i][y].draw(surface)
-                if RoadArr[i][y]==3:
-                    RoadObj[i][y].image=pg.transform.rotate(RoadObj[i][y].image,90)
-                    RoadObj[i][y].draw(surface)
+               # if RoadArr[i][y]==3:
+                #    RoadObj[i][y].draw(surface)
                     #RoadObj[i][y].image=pg.transform.rotate(RoadObj[i][y].image,90)
 
-                if RoadArr[i][y]==1:
-                    CarArr[i][y].draw(surface)
-                    if CarArr[i][y].x < 1600 and CarArr[i][y].y < 800 and surface.get_at((CarArr[i][y].x,CarArr[i][y].y)) != pg.Color("Red") :
-                        CarArr[i][y].x=CarArr[i][y].x+1
-                    else : 
-                        CarArr[i][y].x=0
-                        #CarArr[i][y].y=    
-    #car.draw(surface)
-    #car2.draw(surface)
-    for i in range(0,6,2):
-        pg.draw.circle(surface, pg.Color(boje[i//2]), (semafor[i],semafor[i+1]), 10)
-    delay(10)
-    
-    #else :car.x=car.
+    for i in range(0,5,2):
+        pg.draw.circle(surface, pg.Color(boje[i//2]), (semafor[i],semafor[i+1]), 20)
 
-   # CarArr[].x=car2.x+1
-    #if car2.x==200 :
-     #   boje[0]="Green"
-      #  car.x=car.x+1
+
+    for i in range(0,7):
+        for y in range(0,15):
+                if CarArr[i][y]!=0:
+                    CarArr[i][y].draw(surface)
+                    if CarArr[i][y].x < 1600 and CarArr[i][y].y < 800 :
+                        if  surface.get_at((CarArr[i][y].x+75,CarArr[i][y].y)) != pg.Color("Red") :#and surface.get_at((CarArr[i][y].x+35,CarArr[i][y].y)) != pg.Color("Red") :
+                            CarArr[i][y].x=CarArr[i][y].x+1
+                            temp=CarArr[i][y].x//100+1
+                           # CarLocation[temp][1]==1
+                    else :
+                          CarArr[i][y].x=100
+
+                    #elif CarArr[i][y].x == 1 and CarArr[i][y].y == 699: 
+                     #   CarArr[i][y].x=100
+                   # else   :
+                    #      CarArr[i][y].x=100
     pg.display.update()
     for event in pg.event.get():
            if event.type == pg.KEYDOWN:
