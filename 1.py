52cf2a662208db7f0d89d6a5838a8a183a13c41b
zarko5 <zarkohajder1@gmail.com>
2021-08-25 01:50:54 +0200

    Obrada raskrsnica 1.deo

--- 1.py
+++ 1.py
@@ -1,4 +1,4 @@
-#!/bin/python
+#! /bin/python3
 import pygame as pg
 import numpy as np
 import random
@@ -9,7 +9,7 @@ import random
 # pg.mixer.music.play(-1)
 
 sirina = 1700
-visina = 900
+visina = 1000
 
 
 # CarDirection 0 je desno,1 je gore , 2 je dole
@@ -69,17 +69,23 @@ class Road(object):  # klasa za puteve
         elif (smer == "dole"):
             self.image = pg.transform.rotate(self.image,180)
         elif (smer == "levo"):
-            self.image = pg.transform.rotate(self.image,270)
-        elif (smer == "desno"):
             self.image = pg.transform.rotate(self.image,90)
+        elif (smer == "desno"):
+            self.image = pg.transform.rotate(self.image,270)
 
 class Semafor(object):
     def __init__(self):
         self.x = 0
         self.y = 0
+        self.boja = "Red"
     
-    def draw(self,surface,boja):
-         pg.draw.circle(surface, pg.Color(boja), (self.x, self.y), 20)
+    def draw(self,surface):
+         pg.draw.circle(surface, pg.Color(self.boja), (self.x, self.y), 20)
+
+    def promeni_boju(self):
+        if self.boja == "Green":
+            self.boja = "Red" 
+        else: self.boja = "Green"
 
 
 def osvezi_lokacije(aindx,CarLoc):  # Prolazi kroz sve aute i markira polja na kojima se nalaze kao zauzeta 
@@ -88,9 +94,59 @@ def osvezi_lokacije(aindx,CarLoc):  # Prolazi kroz sve aute i markira polja na k
             CarLoc[i][y]=0
     for index in range(0,aindx):
         CarLoc[CarArr[index].y // 100][CarArr[index].x // 100] = 1
-#    for index in br_semafora:#Menjamo polje na jedan samo ako je boja crvena
+#    for index in br_semafora:#Menjamo polje na jedan samo ako je boja crvena plus semafor moze da se promeni samo ako je slobodno polje
 
-
+def obradi_raskrsnice(matrica,obradi_rotacije):
+    for i in range(0,7):
+        for y in range(0,15):
+            if(matrica[i][y]>2): break
+            if(i == 0):
+                 up = 0
+            else: 
+                up = matrica[i-1][y]
+            if(i == 7):
+                down = 0
+            else: 
+                down = matrica[i+1][y]
+            if(y == 0):
+                left = 0
+            else:
+                left = matrica[i][y-1]
+            if(y == 15):
+                right = 0
+            else:
+                right = matrica[i][y+1]
+            if(up == 2 and down == 2 and left == 1 and right == 1):
+                matrica[i][y] = 4
+            elif(up >= 2 and left>0 and right>0):
+                matrica[i][y] = 5
+            elif(down >= 2 and left>0 and right>0):
+                matrica[i][y] = 5
+                if(obradi_rotacije):
+                    RoadObj[i][y].rotiraj("dole")
+            elif(up >= 2 and down >= 2 and left>0 ):
+                matrica[i][y] = 5
+                if(obradi_rotacije):
+                    RoadObj[i][y].rotiraj("levo")
+            elif(up >= 2 and down >= 2 and right>0):
+                matrica[i][y] = 5
+                if(obradi_rotacije):
+                    RoadObj[i][y].rotiraj("desno")
+            elif(up == 2 and left and matrica[i][y] == 1):
+                matrica[i][y] = 3
+            elif(up == 2 and right and matrica[i][y] == 1):
+                matrica[i][y] = 3
+                if(obradi_rotacije):
+                    RoadObj[i][y].rotiraj("desno")
+            elif(down == 2 and left and matrica[i][y] == 1):
+                matrica[i][y] = 3
+                if(obradi_rotacije):
+                    RoadObj[i][y].rotiraj("levo")
+            elif(down == 2 and right and matrica[i][y] == 1):
+                matrica[i][y] = 3
+                if(obradi_rotacije):
+                    RoadObj[i][y].rotiraj("dole")
+            
 
 semafor = np.array([450, 140, 450, 440, 250, 300, 300, 350])#ovaj  array ce postati niz klasa nakon ubacivanja raskrsnica
 boje = ["Red", "Red", "Red", "Red"]
@@ -110,13 +166,14 @@ pg.display.set_caption("Traffic Jam Simulation")
 RoadArr = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [0, 0, 0, 2, 0, 0, 0, 2, 0, 2, 0, 0, 2, 0, 0, 0],
-    [0, 0, 0, 2, 0, 0, 0, 2, 1, 3, 0, 0, 2, 0, 0, 0],
-    [1, 1, 1, 1, 1, 1, 1, 4, 0, 0, 0, 0, 2, 0, 0, 0],
+    [0, 0, 0, 2, 0, 0, 0, 2, 1, 1, 0, 0, 2, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 2, 0, 0, 0, 0, 2, 0, 0, 0],
     [0, 0, 0, 2, 0, 2, 0, 2, 0, 0, 0, 0, 2, 0, 0, 0],
-    [0, 0, 0, 2, 0, 1, 1, 5, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 2, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 1, 1, 1, 1, 2, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0],
     [0, 0, 0, 2, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0],
 ]
+obradi_raskrsnice(RoadArr,0)
 
 ##
 RoadObj = np.zeros(shape=(8, 16), dtype=Road)  # Objekti puteva
@@ -130,7 +187,7 @@ CarDirection = np.zeros(shape=(br_car + 1), dtype=int)  # smer kretanja automobi
 semafori = np.zeros(shape=(8,17),dtype=Semafor)
 
 auto_index = 0  # ovo treba promenu kako bi uvek bio ispunjen broj automobila br_car, samo se generisu na random lokacijama,
-for i in range(0, 7):
+for i in range(0, 8):
     for y in range(0, 15):
         if RoadArr[i][y] == 1:
             RoadObj[i][y] = Road(1)
@@ -160,7 +217,7 @@ for i in range(0, 7):
             RoadObj[i][y] = Road(5) 
             RoadObj[i][y].y = i * 100 + 100
             RoadObj[i][y].x = y * 100 + 100
-
+obradi_raskrsnice(RoadArr,1)
 
 boja = 0
 Run = True
@@ -169,7 +226,7 @@ random.seed(23617282344)#PRNG eksperiment
 while Run:
     ciklus += 1
     surface.fill((0, 0, 255))
-    for i in range(0, 7):
+    for i in range(0, 8):
         for y in range(0, 15):
             if RoadArr[i][y] != 0:
                 RoadObj[i][y].draw(surface)
